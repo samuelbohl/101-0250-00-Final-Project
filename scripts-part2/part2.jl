e90b7a13222c49241ded805e4e7bef1adae68613
KonaeAkira <long.truong@inf.ethz.ch>
2021-12-16 07:20:17 +0100

    using ParallelStencil

--- scripts-part2/part2.jl
+++ scripts-part2/part2.jl
@@ -1,10 +1,50 @@
+const USE_GPU = false
+
+using ParallelStencil
+using ParallelStencil.FiniteDifferences3D
+@static if USE_GPU
+    @init_parallel_stencil(CUDA, Float64, 3)
+else
+    @init_parallel_stencil(Threads, Float64, 3)
+end
 using Plots
 
-@views function acoustic_2D()
+@parallel function compute_τ!(Vx, Vy, Vz, ∇V, τxx, τyy, τzz, τxy, τyz, τzx, μ, dt, dx, dy, dz)
+    @all(τxx) = @all(τxx) + dt*(2.0*μ*(@d_xa(Vx)/dx) - 1.0/3.0*@all(∇V))
+    @all(τyy) = @all(τyy) + dt*(2.0*μ*(@d_ya(Vy)/dy) - 1.0/3.0*@all(∇V))
+    @all(τzz) = @all(τzz) + dt*(2.0*μ*(@d_za(Vz)/dz) - 1.0/3.0*@all(∇V))
+    @all(τxy) = @all(τxy) + dt*μ*(@d_yi(Vx)/dy + @d_xi(Vy)/dx)
+    @all(τyz) = @all(τyz) + dt*μ*(@d_zi(Vy)/dz + @d_yi(Vz)/dy)
+    @all(τzx) = @all(τzx) + dt*μ*(@d_xi(Vz)/dx + @d_zi(Vx)/dz)
+    return
+end
+
+@parallel function compute_dV!(P, dVxdt, dVydt, dVzdt, τxx, τyy, τzz, τxy, τyz, τzx, ρ, dx, dy, dz)
+    @all(dVxdt) = -1.0/ρ*(@d_xi(P)/dx - @d_xi(τxx)/dx - @d_ya(τxy)/dy - @d_za(τzx)/dz) 
+    @all(dVydt) = -1.0/ρ*(@d_yi(P)/dy - @d_yi(τyy)/dy - @d_za(τyz)/dz - @d_xa(τxy)/dx)
+    @all(dVzdt) = -1.0/ρ*(@d_zi(P)/dz - @d_zi(τzz)/dz - @d_xa(τzx)/dx - @d_ya(τyz)/dy)
+    return
+end
+
+@parallel function compute_V!(Vx, Vy, Vz, ∇V, dVxdt, dVydt, dVzdt, dt, dx, dy, dz)
+    @inn(Vx) = @inn(Vx) + dt*@all(dVxdt)
+    @inn(Vy) = @inn(Vy) + dt*@all(dVydt)
+    @inn(Vz) = @inn(Vz) + dt*@all(dVzdt)
+    @all(∇V) = @d_xa(Vx)/dx + @d_ya(Vy)/dy + @d_za(Vz)/dz
+    return
+end
+
+@parallel function compute_P!(P, dPdt, ∇V, K, dt)
+    @all(dPdt) = -K*@all(∇V)
+    @all(P) = @all(P) + dt*@all(dPdt)
+    return
+end
+
+@views function elastic_wave_3D()
     # Physics
     Lx, Ly, Lz = 10.0, 10.0, 10.0
     ρ          = 1.0
-    μ          = 10.0
+    μ          = 1.0
     K          = 1.0
     ttot       = 20.0
     # Numerics
@@ -17,53 +57,35 @@ using Plots
     xc, yc, zc = LinRange(dx/2, Lx-dx/2, nx), LinRange(dy/2, Ly-dy/2, ny), LinRange(dz/2, Lz-dz/2, nz)
     # Array initialisation
     P      = exp.([-1.0*((x-Lx/2)^2 + (y-Ly/2)^2 + (z-Lz/2)^2) for x=xc, y=yc, z=zc])
-    dPdt   = zeros(Float64, nx  , ny  , nz  )
+    dPdt   = @zeros(nx, ny, nz)
 
-    Vx     = zeros(Float64, nx+1, ny  , nz  )
-    Vy     = zeros(Float64, nx  , ny+1, nz  )
-    Vz     = zeros(Float64, nx  , ny  , nz+1)
+    Vx = @zeros(nx+1,ny  ,nz  )
+    Vy = @zeros(nx  ,ny+1,nz  )
+    Vz = @zeros(nx  ,ny  ,nz+1)
+    ∇V = @zeros(nx  ,ny  ,nz  )
+    
+    dVxdt = @zeros(nx-1, ny-2, nz-2)
+    dVydt = @zeros(nx-2, ny-1, nz-2)
+    dVzdt = @zeros(nx-2, ny-2, nz-1)
 
-    dVxdt  = zeros(Float64, nx-1, ny-2, nz-2)
-    dVydt  = zeros(Float64, nx-2, ny-1, nz-2)
-    dVzdt  = zeros(Float64, nx-2, ny-2, nz-1)
-
-    τxx    = zeros(Float64, nx  , ny  , nz  )
-    τyy    = zeros(Float64, nx  , ny  , nz  )
-    τzz    = zeros(Float64, nx  , ny  , nz  )
-
-    τxy    = zeros(Float64, nx-1, ny-1, nz-2)
-    τyz    = zeros(Float64, nx-2, ny-1, nz-1)
-    τzx    = zeros(Float64, nx-1, ny-2, nz-1)
-
-    ∇V     = zeros(Float64, nx  , ny  , nz  )
+    τxx = @zeros(nx  ,ny  ,nz  )
+    τyy = @zeros(nx  ,ny  ,nz  )
+    τzz = @zeros(nx  ,ny  ,nz  )
+    τxy = @zeros(nx-1,ny-1,nz-2)
+    τyz = @zeros(nx-2,ny-1,nz-1)
+    τzx = @zeros(nx-1,ny-2,nz-1)
     # Time loop
     for it = 1:nt
-        τxx   .= τxx .+ dt*(2.0.*μ.*(diff(Vx,dims=1)/dx .- 1/3 .*∇V))
-        τyy   .= τyy .+ dt*(2.0.*μ.*(diff(Vy,dims=2)/dy .- 1/3 .*∇V))
-        τzz   .= τzz .+ dt*(2.0.*μ.*(diff(Vz,dims=3)/dz .- 1/3 .*∇V))
-
-        τxy   .= τxy .+ dt*(μ.*(diff(Vx[2:end-1,:,2:end-1],dims=2)/dy .+ diff(Vy[:,2:end-1,2:end-1],dims=1)/dx))
-        τyz   .= τyz .+ dt*(μ.*(diff(Vy[2:end-1,2:end-1,:],dims=3)/dz .+ diff(Vz[2:end-1,:,2:end-1],dims=2)/dy))
-        τzx   .= τzx .+ dt*(μ.*(diff(Vz[:,2:end-1,2:end-1],dims=1)/dx .+ diff(Vx[2:end-1,2:end-1,:],dims=3)/dz))
-
-        dVxdt .= .-1.0./ρ.*(diff(P[:,2:end-1,2:end-1],dims=1)./dx .- diff(τxx[:,2:end-1,2:end-1],dims=1)./dx .- diff(τxy,dims=2)./dy .- diff(τzx,dims=3)./dz)
-        dVydt .= .-1.0./ρ.*(diff(P[2:end-1,:,2:end-1],dims=2)./dy .- diff(τyy[2:end-1,:,2:end-1],dims=2)./dy .- diff(τxy,dims=1)./dx .- diff(τyz,dims=3)./dz)
-        dVzdt .= .-1.0./ρ.*(diff(P[2:end-1,2:end-1,:],dims=3)./dz .- diff(τzz[2:end-1,2:end-1,:],dims=3)./dz .- diff(τyz,dims=2)./dy .- diff(τzx,dims=1)./dx)
-
-        Vx[2:end-1,2:end-1,2:end-1] .= Vx[2:end-1,2:end-1,2:end-1] .+ dt.*dVxdt
-        Vy[2:end-1,2:end-1,2:end-1] .= Vy[2:end-1,2:end-1,2:end-1] .+ dt.*dVydt
-        Vz[2:end-1,2:end-1,2:end-1] .= Vz[2:end-1,2:end-1,2:end-1] .+ dt.*dVzdt
-        ∇V    .= diff(Vx,dims=1)./dx .+ diff(Vy,dims=2)./dy .+ diff(Vz,dims=3)./dz
-
-        dPdt  .= .-K.*∇V
-        P     .= P .+ dt.*dPdt
-
+        @parallel compute_τ!(Vx, Vy, Vz, ∇V, τxx, τyy, τzz, τxy, τyz, τzx, μ, dt, dx, dy, dz)
+        @parallel compute_dV!(P, dVxdt, dVydt, dVzdt, τxx, τyy, τzz, τxy, τyz, τzx, ρ, dx, dy, dz)
+        @parallel compute_V!(Vx, Vy, Vz, ∇V, dVxdt, dVydt, dVzdt, dt, dx, dy, dz)
+        @parallel compute_P!(P, dPdt, ∇V, K, dt)
         if it % nout == 0
-            opts = (aspect_ratio=1, xlims=(xc[1], xc[end]), ylims=(yc[1], yc[end]), clims=(-0.1, 0.4), c=:davos, xlabel="Lx", ylabel="Ly", title="time = $(round(it*dt, sigdigits=3))")
+            opts = (aspect_ratio=1, xlims=(xc[1], xc[end]), ylims=(yc[1], yc[end]), clims=(-0.15, 0.65), c=:davos, xlabel="Lx", ylabel="Ly", title="time = $(round(it*dt, sigdigits=3))")
             display(heatmap(xc, yc, P[:,:,cld(nz,2)]'; opts...))
         end
     end
     return
 end
 
-acoustic_2D()
+elastic_wave_3D()
