7482f8d4f2745d518a9484257b25b7a667e78bbe
samuelbohl <samuel.bohl@gmail.com>
2021-12-23 10:19:44 +0100

    updated test files

--- test/part1.jl
+++ test/part1.jl
@@ -1,36 +1,27 @@
 # Testing part 1
 
-#include("../scripts-part1/part1.jl") # modify to include the correct script
-
+# Initialize Globals
 const USE_GPU = false
 const BENCHMARK = false
 const VISUALIZE = false
 
+# Run the XPU script
 include("../scripts-part1/diffusion3D_xpu.jl")
-
-
 xc, H = diffusion_3D(32)
 
-# Add unit and reference tests
-
-# @testset "Testset nx,ny = [64, 128, 256, 512]" begin
-#     C64 = diffusion_2D(64)[[5, Int(cld(0.6*64,1)), 54], Int(cld(0.5*64, 1))]'
-#     C128 = diffusion_2D(128)[[5, Int(cld(0.6*128,1)), 118], Int(cld(0.5*128, 1))]'
-#     C256 = diffusion_2D(256)[[5, Int(cld(0.6*256,1)), 246], Int(cld(0.5*256, 1))]'
-#     C512 = diffusion_2D(512)[[5, Int(cld(0.6*512,1)), 502], Int(cld(0.5*512, 1))]'
-#     @test C64 ≈ [1.28961441675812e-6 0.3403434055248243 0.000226725154067358]
-#     @test C128 ≈ [1.42876853096198e-7 0.3606848631942946 2.784022638919167e-6]
-#     @test C256 ≈ [3.82994869422046e-8 0.3515100977539851 2.070629144549965e-7]
-#     @test C512 ≈ [1.56975129887789e-8 0.3467239448747831 4.938759153492403e-8]
-# end
-
+# Run the Multi XPU script
+ParallelStencil.@reset_parallel_stencil()
+include("../scripts-part1/diffusion3D_multixpu.jl")
+xc2, H2 = diffusion_3D(32)
 
 # Reference test using ReferenceTests.jl
 "Compare all dict entries"
 comp(d1, d2) = keys(d1) == keys(d2) && all([ isapprox(v1, v2; atol = 1e-5) for (v1,v2) in zip(values(d1), values(d2))])
 inds = Int.(ceil.(LinRange(1, length(xc), 12)))
-d = Dict(:X=> xc[inds], :H=>H[inds, inds, 15])
+d_xpu = Dict(:X=> xc[inds], :H=>H[inds, inds, 15])
+d_mxpu = Dict(:X=> xc2[inds], :H=>H2[inds, inds, 15])
 
-@testset "Ref-file" begin
-    @test_reference "reftest-files/test_1.bson" d by=comp
+@testset "Reference Test - Diffusion 3D" begin
+    @test_reference "reftest-files/test_1.bson" d_xpu by=comp
+    @test_reference "reftest-files/test_1.bson" d_mxpu by=comp
 end
--- test/part2.jl
+++ test/part2.jl
@@ -1,14 +1,14 @@
 # Testing part 2
 
-include("../scripts-part2/elastic_wave_3D_testing.jl") # modify to include the correct script
+include("../scripts-part2/elastic_wave_3D_testing.jl")
 
-# Add unit and reference tests
 
+# Reference test using ReferenceTests.jl
 "Compare all dict entries"
 comp(d1, d2) = keys(d1) == keys(d2) && all([ isapprox(v1, v2; atol = 1e-5) for (v1,v2) in zip(values(d1), values(d2))])
 inds = Int.(ceil.(LinRange(1, length(xc), 12)))
 d = Dict(:X=> xc[inds], :H=>P[inds, inds, 15])
 
-@testset "Ref-file" begin
+@testset "Reference Test - Elastic Wave 3D" begin
     @test_reference "reftest-files/test_2.bson" d by=comp
 end
